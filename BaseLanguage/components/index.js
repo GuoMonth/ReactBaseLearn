29ab5468191c451ba18f543100109ce4643c3c79
guoshenga <guoshenga@digitalchina.com>
2020-08-24 18:29:21 +0800

    ok

---
+++ BaseLanguage/components/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const str = 'Hi';
+
+const re = ()=> ReactDOM.render(
+<p>{str}</p>,rootE1
+);
+
+re();
---
+++ BaseLanguage/test.ts
@@ -0,0 +1,1 @@
+const h : string = "Hi";
